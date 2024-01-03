ebda9d4ebb69bb5975afed187f07ff637f118541
Tiago829 <stfoliveira298@gmail.com>
2024-01-03 16:54:47 +0000

    projeto compilado sem erros

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.android.tools.build:gradle:7.2.1")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
