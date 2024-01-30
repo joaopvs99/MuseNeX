6420612444184e87d210727f2360354cf0c30d48
RuiSilvaVPA <ruisilvaipca@gmail.com>
2024-01-30 19:11:13 +0000

    updating QRCode feature

---
+++ app/src/main/java/com/ipca_project/musenex/testedelayouts.kt
@@ -0,0 +1,126 @@
+
+package com.ipca_project.musenex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gtappdevelopers.kotlingfgproject.CategoryAdapter
+import com.gtappdevelopers.kotlingfgproject.EventsAdapter
+import model.Event
+import model.Museum
+import viewModels.DiscoveryViewModel
+
+// testar layouts em construção ainda sem classe criada...
+
+class testedelayouts : AppCompatActivity() {
+
+    var teste : Int = 0
+
+    private lateinit var EventsSeeAllCard: RecyclerView
+
+    //Adapters
+    private lateinit var eventsAdapter: EventsAdapter
+
+
+
+    //lists
+    private lateinit var EventList: ArrayList<Event>
+    private lateinit var MuseumList: ArrayList<Museum>
+
+    // dataBase
+    var viewModel = DiscoveryViewModel()
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.see_all)
+
+        viewModel.fetchDiscovery()
+
+        //toolbar settings
+        setSupportActionBar(findViewById(R.id.seeAllToolbar))
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+
+        //call variables
+        EventsSeeAllCard = findViewById(R.id.seeAllEvents)
+
+        when(teste){
+
+            0 -> {
+                getSupportActionBar()?.setTitle("Todos os Eventos")
+
+                viewModel.events.observe(this, Observer { events ->
+                    EventList = ArrayList()
+                    for (searchEvent in events){
+                        EventList.add(
+                            Event(
+                                eventId = searchEvent.eventId,
+                                name = searchEvent.name,
+                                date_event_beg = searchEvent.date_event_beg,
+                                date_event_end = searchEvent.date_event_end,
+                                description = searchEvent.description,
+                                museumId = searchEvent.museumId,
+                                galeryEvent = searchEvent.galeryEvent,
+                                categoryId = searchEvent.categoryId
+                            )
+                        )
+
+                    }
+
+                    val layoutManager = GridLayoutManager(this, 2)
+                    EventsSeeAllCard.layoutManager = layoutManager
+
+                    //start adapter
+                    //eventsAdapter = EventsAdapter(EventList, MuseumList,this,this)
+
+                    // turn adapter to recycleView
+                    EventsSeeAllCard.adapter = eventsAdapter
+
+                    eventsAdapter.notifyDataSetChanged()
+                    }
+                )
+            }
+
+            1 -> { getSupportActionBar()?.setTitle("Todas as Obras")}
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
+<<<<<<< Updated upstream
+=======
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
+>>>>>>> Stashed changes
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
